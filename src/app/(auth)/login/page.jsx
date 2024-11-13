2cc203ca7bdd28ecb3f3ac0b1519fb82c7c305c8
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-13 22:03:21 +0900

    init

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginClient from "@/app/(auth)/login/LoginClient";
+
+const Login = () => {
+
+    return (
+        <LoginClient/>
+    )
+}
+
+export default Login
---
+++ src/app/(auth)/reset/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Reset = () => {
+    return (
+        <div>asd</div>
+    )
+}
+
+export default Reset

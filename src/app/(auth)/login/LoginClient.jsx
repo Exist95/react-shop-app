ed4ce23e957eded0b60d7da9d6841b0d5a408fb9
노종열 <96723716+Exist95@users.noreply.github.com>
2024-11-14 02:33:40 +0900

    commit test

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -1,55 +1,46 @@
-'use client'
-import React, {useState} from 'react';
-import Image from "next/image";
-import LogoPath from "@/assets/colorful.svg"
-import {useRouter} from "next/navigation";
+'use client';
+import React, { useState } from 'react';
+import Image from 'next/image';
+import LogoPath from '@/assets/colorful.svg';
+import { useRouter } from 'next/navigation';
 
-import styles from "./Auth.module.scss";
+import styles from './Auth.module.scss';
 
 const LoginClient = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAutoLogin, setIsAutoLogin] = useState(false);
+  const router = useRouter();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [isAutoLogin, setIsAutoLogin] = useState(false);
-    const router = useRouter();
+  const redirectUser = () => {
+    router.push('/');
+  };
 
-    const redirectUser = () => {
-        router.push('/');
-    }
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
 
-    const loginUser = (e) => {
-        e.preventDefault();
-        setIsLoading(true);
+  const signInWithGoogle = () => {
+    //커밋테스트
+  };
 
-    }
+  return (
+    <section className={styles.page}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Image src={LogoPath} alt='logo' />
+        </h1>
 
-    const signInWithGoogle = () => {
+        <form className={styles.form} onSubmit={loginUser}>
+          <div className={styles.group}></div>
 
-    }
-
-
-    return (
-        <section className={styles.page}>
-            <div className={styles.container}>
-                <h1 className={styles.logo}>
-                    <Image src={LogoPath} alt='logo' />
-                </h1>
-
-                <form className={styles.form} onSubmit={loginUser}>
-
-                    <div className={styles.group}>
-
-                    </div>
-
-                    <div className={styles.buttonGroup}>
-
-                    </div>
-
-                </form>
-            </div>
-        </section>
-    );
+          <div className={styles.buttonGroup}></div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default LoginClient;
